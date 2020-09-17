0e89fd834feff470a868e1c29b2b0ee42e868cca
Aditya Bhamidipati <adityabhamidipati@MacBook-Pro.fios-router.home>
2020-09-16 20:44:46 -0400

    GPPF Update

---
+++ pf_filt.py
@@ -0,0 +1,72 @@
+import numpy as np
+from numpy.random import uniform, randn
+from numpy.linalg import norm
+import scipy.stats
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def create_particles(N, x, y):
+    particles = np.empty((N, 2))
+    particles[:, 0] = uniform(x[0], x[1], size=N)
+    particles[:, 1] = uniform(y[0], y[1], size=N)
+    return particles
+
+
+def predict(particles, control_input, std, dt):
+    num_of_particles = len(particles)
+
+    # Headings
+
+    xdist = (control_input[0] * dt) + (randn(num_of_particles) * std[0])
+    ydist = (control_input[1] * dt) + (randn(num_of_particles) * std[1])
+
+    particles[:, 0] += xdist
+    particles[:, 1] += ydist
+
+
+def update(particles, weights, observation, sensor_std, landmarks):
+    weights.fill(1.)
+
+    for i, landmark in enumerate(landmarks):
+        distance = norm(particles[:, 0:2] - landmark, axis=1)
+        pdf_in = observation[i]
+        pdfs = scipy.stats.norm(distance, sensor_std).pdf(pdf_in)
+        weights *= pdfs
+
+    weights += 1.e-300
+    weights /= sum(weights)  # normalize
+
+
+def gaussian_process_reg(x, y):
+    kernel = 1.0 * RBF(1.0)
+    gp = GaussianProcessRegressor(kernel=kernel, random_state=0)
+    gp.fit(x, y)
+    y_mean = gp.predict(x, return_std=True)
+    return y_mean
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(x[:, 0], x[:, 1], y_mean, marker='o', color='orange')
+    #
+    # ax.set_xlabel('Part X ')
+    # ax.set_ylabel('Part Y')
+    # ax.set_zlabel('Weights')
+
+    # plt.show()
+
+
+def effective_particles(gp_weights):
+    return 1. / np.sum(np.square(gp_weights))
+
+
+def estimate(particles, weights):
+    mean = np.average(particles[:], weights=weights, axis=0)
+    return mean
+
+
+def resample(particles, weights, indexes):
+    particles[:] = particles[indexes]
+    weights[:] = weights[indexes]
+    weights.fill(1.0 / len(weights))
---
+++ testing.py
@@ -0,0 +1,50 @@
+import pf_filt as pf
+import numpy as np
+from numpy.random import uniform, randn
+from numpy.linalg import norm
+from filterpy.monte_carlo import systematic_resample
+from matplotlib import pyplot as plt
+
+N = 500
+x_dim = (0, 20)
+y_dim = (0, 20)
+control_input = (1, 1)
+std = (.1, .1)
+dt = 1
+landmarks = np.array([[-1, 0], [2, 3], [-1, 15]])
+# Test Parameters
+num_of_iterations = 20
+sensor_std = .2
+
+particles = pf.create_particles(N=N, x=x_dim, y=y_dim)
+print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+weights = np.zeros(N)
+
+robot_position = np.array([0., 0.])
+for iteration in range(num_of_iterations):
+    robot_position += (1, 1)
+    diff = landmarks - robot_position
+    observation = (norm(diff, axis=1) +
+                   (randn(len(landmarks)) * sensor_std))
+
+    pf.predict(particles=particles, control_input=control_input, std=std, dt=dt)
+
+    pf.update(particles=particles, weights=weights, observation=observation, sensor_std=sensor_std,
+              landmarks=landmarks)
+
+    mean = pf.estimate(particles, weights)
+    p1 = plt.scatter(robot_position[0], robot_position[1], marker='+',
+                     color='k', s=180, lw=3)
+    p2 = plt.scatter(mean[0], mean[1], marker='s', color='r')
+
+    # Initial Resample
+    if pf.effective_particles(weights) < N / 2:
+        indexes = systematic_resample(weights)
+        pf.resample(particles, weights, indexes)
+
+    # print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
+plt.legend([p1, p2], ['Actual', 'PF'], loc=4, numpoints=1)
+plt.show()
+
+
